369650da503122a72e5136877cea4bb755133a02
xXlittlestarXx <lilyanfauber@gmail.com>
2025-04-14 12:59:56 -0400

    Building second activity java

--- app/src/main/java/com/example/winkylite/SecondActivity.java
+++ app/src/main/java/com/example/winkylite/SecondActivity.java
@@ -1,4 +1,30 @@
 package com.example.winkylite;
 
-public class SecondActivity {
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button addPetButton;
+    private DBHandler dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        addPetButton = findViewById(R.id.AddPetButton);
+        addPetButton.setOnClickListener(v-> {
+
+        });
+    }
+
 }
