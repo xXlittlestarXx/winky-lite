c08d6b87670927ed29b5992373264d67a8a78cae
xXlittlestarXx <lilyanfauber@gmail.com>
2025-04-21 12:56:35 -0400

    build pet class

--- app/src/main/java/com/example/winkylite/activities/ThirdActivity_AddPetForm.java
+++ app/src/main/java/com/example/winkylite/activities/ThirdActivity_AddPetForm.java
@@ -3,15 +3,33 @@ package com.example.winkylite.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.winkylite.database.DBHandler;
+//import com.example.winkylite.database.DBHandler;
 import com.example.winkylite.R;
 
 public class ThirdActivity_AddPetForm extends AppCompatActivity {
-    private Button backButton;
-    private DBHandler dbHelper;
+    /* VARIABLES FROM activity_third*/
+
+    private Button backButton, submitButton;
+    private RadioButton ageMonths, ageYears, typeCat, typeDog,
+            genderMale, genderFemale, fixedYes, fixedNo;
+
+    private EditText petName, petAge, petCWeight, petGWeight;
+    private CheckBox setGoalWeight;
+
+    private SeekBar petActivityLevel;
+
+    /* DATABASE HANDLER*/
+    /* only db handler class can interact with database to limit
+    overloading and overriding of data*/
+    /*interacts with Pets class which interacts with db handler instead*/
+    //private DBHandler dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
--- app/src/main/java/com/example/winkylite/models/Pets.java
+++ app/src/main/java/com/example/winkylite/models/Pets.java
@@ -1,4 +1,65 @@
 package com.example.winkylite.models;
 
 public class Pets {
+
+    /* VARIABLES */
+    private String petName, ageUnit, petGender, petType;
+    /* months / years  ; male/female  ; cat/dog */
+    private boolean isFixed, hasGoalWeight;
+    /* Yes/No */
+    private int petAge, activityLevel;
+    /* 0, 1, or 2 */
+    private double petCurrentWeight, petGoalWeight;
+
+    /* CONSTRUCTOR */
+    public Pets (String petName, String ageUnit, String petGender, String petType,
+                 boolean isFixed, boolean hasGoalWeight,
+                 int petAge, int activityLevel,
+                 double petCurrentWeight, double petGoalWeight){
+        /*Strings*/
+        this.petName = petName;
+        this.ageUnit = ageUnit;
+        this.petGender = petGender;
+        this.petType = petType;
+
+        /*booleans*/
+        this.isFixed = isFixed;
+        this.hasGoalWeight = hasGoalWeight;
+
+        /*ints*/
+        this.petAge = petAge;
+        this.activityLevel = activityLevel;
+
+        /*doubles*/
+        this.petCurrentWeight = petCurrentWeight;
+        this.petGoalWeight = petGoalWeight;
+
+
+    }
+
+    /* GETTERS */
+
+    /* STRINGS */
+    public String getPetName () {return petName;}
+    public String getAgeUnit() {return ageUnit;}
+    public String getPetGender () {return petGender;}
+    public String getPetType () {return petType;}
+
+    /* BOOLEANS */
+    public boolean getIsFixed () {return isFixed;}
+    public boolean getHasGoalWeight () {return hasGoalWeight;}
+
+
+    /* INTS */
+    public int getPetAge () {return petAge;}
+    public int getPetActivityLevel () {return activityLevel;}
+
+    /* DOUBLES */
+
+    public double getPetCurrentWeight() {return petCurrentWeight;}
+    public double getPetGoalWeight() {return petGoalWeight;}
+
+
 }
+
+
