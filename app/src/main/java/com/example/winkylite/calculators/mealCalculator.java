21198678736f63f9339c8423b1d1d34af10eeca6
xXlittlestarXx <lilyanfauber@gmail.com>
2025-04-28 17:46:05 -0400

    fixed bug; build mealcalc
    fixed bug with activity five - meal item template was a constraint layout instead of a linear layout. changed it and it appears now when prompted
    
    building calc meal to calculate average of nutrients across items in meal

--- app/src/main/java/com/example/winkylite/calculators/mealCalculator.java
+++ app/src/main/java/com/example/winkylite/calculators/mealCalculator.java
@@ -5,12 +5,34 @@ import com.example.winkylite.models.mealItem;
 import java.util.List;
 
 public class mealCalculator {
-    public double totalKcal;
-    public double totalMoisture;
-    public double totalFats;
-    public double totalProtein;
+    public double averageKcal;
+    public double averageMoisture;
+    public double averageFats;
+    public double averageProtein;
     public static double[] calculateMeal(List<mealItem> mealItemList) {
-        double[] i = {0};
-        return i;
+        double totalKcal = 0;
+        double totalMoisture = 0;
+        double totalFats = 0;
+        double totalProtein = 0;
+
+        if (mealItemList == null || mealItemList.isEmpty()) {
+            return new double[]{0, 0, 0, 0};
+        }
+
+        for (mealItem item : mealItemList) {
+            totalKcal += item.getKcal();
+            totalMoisture += item.getMoisture();
+            totalFats += item.getFats();
+            totalProtein += item.getProtein();
+        }
+
+        int count = mealItemList.size();
+
+        double averageKcal = totalKcal / count;
+        double averageMoisture = totalMoisture / count;
+        double averageFats = totalFats / count;
+        double averageProtein = totalProtein / count;
+
+        return new double[]{averageKcal, averageMoisture, averageFats, averageProtein};
     }
 }
